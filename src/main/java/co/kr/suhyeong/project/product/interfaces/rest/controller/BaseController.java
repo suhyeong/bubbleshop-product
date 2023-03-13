0092dffb3f0721254626aa9acb293e32336919d6
suhyeong <lsg1454@gmail.com>
2023-03-13 23:10:16 +0900

    First Commit Test

---
+++ src/main/java/co/kr/suhyeong/project/product/interfaces/rest/controller/BaseController.java
@@ -0,0 +1,4 @@
+package co.kr.suhyeong.project.product.interfaces.rest.controller;
+
+public class BaseController {
+}
---
+++ src/main/java/co/kr/suhyeong/project/product/interfaces/rest/controller/ProductController.java
@@ -0,0 +1,7 @@
+package co.kr.suhyeong.project.product.interfaces.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+}
