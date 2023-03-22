7a7242528cf1bace7429d4c0f19dc775b044c412
suhyeong <lsg1454@gmail.com>
2023-03-22 23:56:08 +0900

    Controller RestFul API getProductList, getProduct Add

--- src/main/java/co/kr/suhyeong/project/product/interfaces/rest/controller/ProductController.java
+++ src/main/java/co/kr/suhyeong/project/product/interfaces/rest/controller/ProductController.java
@@ -1,23 +1,13 @@
 package co.kr.suhyeong.project.product.interfaces.rest.controller;
 
 import co.kr.suhyeong.project.product.interfaces.rest.dto.CreateProductReqDto;
-import co.kr.suhyeong.project.product.interfaces.rest.validator.CreateProductReqDtoValidation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
-import static co.kr.suhyeong.project.product.interfaces.rest.controller.ProductUrl.PRODUCTS;
-import static co.kr.suhyeong.project.product.interfaces.rest.controller.ProductUrl.PRODUCT_DEFAULT_URL;
+import static co.kr.suhyeong.project.product.interfaces.rest.controller.ProductUrl.*;
 
 @Slf4j
 @RestController
@@ -26,6 +16,18 @@ public class ProductController extends BaseController {
 
     @PostMapping(PRODUCTS)
     public ResponseEntity<Void> createProduct(@RequestBody @Validated CreateProductReqDto reqDto) {
+
+        return new ResponseEntity<>(null, getSuccessHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping(PRODUCTS)
+    public ResponseEntity<Void> getProductList() {
+
+        return new ResponseEntity<>(null, getSuccessHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping(PRODUCT)
+    public ResponseEntity<Void> getProduct(@PathVariable String productId) {
 
         return new ResponseEntity<>(null, getSuccessHeaders(), HttpStatus.OK);
     }
--- src/main/java/co/kr/suhyeong/project/product/interfaces/rest/controller/ProductUrl.java
+++ src/main/java/co/kr/suhyeong/project/product/interfaces/rest/controller/ProductUrl.java
@@ -3,4 +3,5 @@ package co.kr.suhyeong.project.product.interfaces.rest.controller;
 public class ProductUrl {
     public static final String PRODUCT_DEFAULT_URL = "/product/v1";
     public static final String PRODUCTS = "/products";
+    public static final String PRODUCT = PRODUCTS + "/{productId}";
 }
