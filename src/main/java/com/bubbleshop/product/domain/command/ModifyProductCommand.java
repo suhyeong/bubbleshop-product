ba50cbebafa4cc548acef86911704df5f238964a
suhyeong <lsg1454@gmail.com>
2025-08-22 10:37:53 +0900

    [FIX] 상품 포인트 추가 및 수정 작업

--- src/main/java/com/bubbleshop/product/domain/command/ModifyProductCommand.java
+++ src/main/java/com/bubbleshop/product/domain/command/ModifyProductCommand.java
@@ -1,6 +1,7 @@
 package com.bubbleshop.product.domain.command;
 
 import com.bubbleshop.product.domain.constant.FeatureType;
+import com.bubbleshop.product.domain.constant.PointType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,6 +23,7 @@ public class ModifyProductCommand {
     private int discount;
     private boolean isSale;
     private Set<ProductOption> options;
+    private Set<ProductPoint> points;
 
     @Builder
     @Getter
@@ -32,5 +34,14 @@ public class ModifyProductCommand {
         private String name;
         private int stockCnt;
         private boolean isDefaultOption;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ProductPoint {
+        private PointType productType;
+        private int savePoint;
     }
 }
--- src/main/java/com/bubbleshop/product/interfaces/rest/transform/ModifyProductCommandDTOAssembler.java
+++ src/main/java/com/bubbleshop/product/interfaces/rest/transform/ModifyProductCommandDTOAssembler.java
@@ -2,23 +2,31 @@ package com.bubbleshop.product.interfaces.rest.transform;
 
 import com.bubbleshop.product.domain.command.ModifyProductCommand;
 import com.bubbleshop.product.domain.constant.FeatureType;
+import com.bubbleshop.product.domain.constant.PointType;
+import com.bubbleshop.product.domain.constant.ProductImageCode;
 import com.bubbleshop.product.interfaces.rest.dto.ModifyProductOptionReqDto;
+import com.bubbleshop.product.interfaces.rest.dto.ModifyProductPointReqDto;
 import com.bubbleshop.product.interfaces.rest.dto.ModifyProductReqDto;
 import org.mapstruct.*;
 
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR, imports = {PointType.class})
 public abstract class ModifyProductCommandDTOAssembler {
 
     @Mappings({
             @Mapping(target = "options", source = "reqDto.options", qualifiedByName = "ModifyProductCommand.Set<ProductOption>"),
+            @Mapping(target = "points", source = "reqDto.points", qualifiedByName = "ModifyProductCommand.Set<ProductPoint>"),
             @Mapping(target = "featureTypes", ignore = true)
     })
     public abstract ModifyProductCommand toCommand(String productCode, ModifyProductReqDto reqDto);
 
     @Named("ModifyProductCommand.Set<ProductOption>")
     public abstract ModifyProductCommand.ProductOption toOptionCommand(ModifyProductOptionReqDto reqDto);
+
+    @Named("ModifyProductCommand.Set<ProductPoint>")
+    @Mapping(target = "productType", expression = "java( PointType.find(reqDto.getPointTypeCode()) )")
+    public abstract ModifyProductCommand.ProductPoint toPointCommand(ModifyProductPointReqDto reqDto);
 
     @AfterMapping
     protected void afterMappingToCommand(
